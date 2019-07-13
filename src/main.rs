097548e62f928f34c71ba820f71ef9a60d4b4041
Ryp <ryp.sqrt@gmail.com>
2019-07-13 16:41:37 +0200

    gl: remove vertex buffer, use hardcoded vertices.
    Split fragment shader processing in separate routine.
    Minor cleanups in main.rs.

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,45 @@ use clap::{Arg, App};
 #[macro_use]
 extern crate glium;
 
-#[derive(Copy, Clone)]
-struct Vertex {
-    position: [f32; 2],
+fn load_fragment_code(fragment_path: &str) -> String
+{
+    let shadertoy_preamble = r#"
+        #version 300 es
+        precision highp float;
+
+        uniform vec3 iResolution;
+        uniform float iTime;
+        // uniform float iTimeDelta;
+        // uniform float iFrame;
+        // uniform float iChannelTime[4];
+        // uniform vec4 iMouse;
+        // uniform vec4 iDate;
+        // uniform float iSampleRate;
+        // uniform vec3 iChannelResolution[4];
+        // uniform samplerXX iChanneli;
+        out vec4 glFragColor;
+        #line 0
+    "#;
+
+    let shadertoy_postamble = r#"
+        void main() {
+            glFragColor.w = 1.0;
+            mainImage(glFragColor, gl_FragCoord.xy);
+            //mainImage(gl_FragColor, gl_FragCoord.xy);
+        }
+    "#;
+
+    // Read whole fragment shader file
+    let fragment_shader_string = std::fs::read(fragment_path).expect("Unable to read file");
+
+    // Add ShaderToy specific code
+    let mut fragment_code = String::new();
+    fragment_code.push_str(shadertoy_preamble);
+    fragment_code.push_str(&String::from_utf8(fragment_shader_string).unwrap());
+    fragment_code.push_str(shadertoy_postamble);
+
+    fragment_code
 }
-implement_vertex!(Vertex, position);
 
 fn main()
 {
@@ -29,58 +63,21 @@ fn main()
     let wb = glium::glutin::WindowBuilder::new();
     let cb = glium::glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &events_loop).unwrap();
-
-    let vertex1 = Vertex { position: [-1.0, 1.0] };
-    let vertex2 = Vertex { position: [ 3.0, 1.0] };
-    let vertex3 = Vertex { position: [ -1.0, -3.0] };
-    let shape = vec![vertex1, vertex2, vertex3];
-
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
     let vertex_shader_src = r#"
         #version 300 es
         precision highp float;
 
-        in vec2 position;
-
         void main() {
-            gl_Position = vec4(position, 0.0, 1.0);
+            vec2 vertices[3];
+            vertices[0] = vec2(-1.0, 1.0);
+            vertices[1] = vec2( 3.0, 1.0);
+            vertices[2] = vec2(-1.0,-3.0);
+            gl_Position = vec4(vertices[gl_VertexID], 0.0, 1.0);
         }
     "#;
-
-    let fragment_shader_preamble = r#"
-        #version 300 es
-        precision highp float;
-
-        uniform vec3 iResolution;
-        uniform float iTime;
-        // uniform float iTimeDelta;
-        // uniform float iFrame;
-        // uniform float iChannelTime[4];
-        // uniform vec4 iMouse;
-        // uniform vec4 iDate;
-        // uniform float iSampleRate;
-        // uniform vec3 iChannelResolution[4];
-        // uniform samplerXX iChanneli;
-        out vec4 glFragColor;
-        #line 0
-    "#;
-
-    let fragment_shader_postamble = r#"
-        void main() {
-            glFragColor.w = 1.0;
-            mainImage(glFragColor, gl_FragCoord.xy);
-            //mainImage(gl_FragColor, gl_FragCoord.xy);
-        }
-    "#;
-
-    let fragment_shader_string = std::fs::read(&fragment_path).expect("Unable to read file");
-    //let mut fragment_code = String::from_utf8(fragment_shader_string).unwrap();
-    let mut fragment_code = String::new();
-    fragment_code.push_str(fragment_shader_preamble);
-    fragment_code.push_str(&String::from_utf8(fragment_shader_string).unwrap());
-    fragment_code.push_str(fragment_shader_postamble);
+    let fragment_code = load_fragment_code(fragment_path);
 
     let program = match glium::Program::from_source(&display, vertex_shader_src, &fragment_code, None) {
         Result::Ok(val) => val,
@@ -91,25 +88,29 @@ fn main()
     };
 
     let time = SystemTime::now();
-    let mut closed = false;
+    let mut is_running = true;
 
-    while !closed {
+    while is_running {
         let mut target = display.draw();
+
+        let framebuffer_extent = display.get_framebuffer_dimensions();
 
         let time_msecs = time.elapsed().expect("Time error").as_millis();
         let time_secs: f32 = time_msecs as f32 / 1000.0;
 
-        let (x, y) = display.get_framebuffer_dimensions();
+        let uniforms = uniform! {
+            iResolution: [framebuffer_extent.0 as f32, framebuffer_extent.1 as f32, 1f32],
+            iTime: time_secs,
+        };
 
         target.clear_color(0.0, 0.0, 1.0, 1.0);
-        let uniforms = uniform! {iTime: time_secs, iResolution: [x as f32, y as f32, 0f32]};
-        target.draw(&vertex_buffer, &indices, &program, &uniforms, &Default::default()).unwrap();
+        target.draw(glium::vertex::EmptyVertexAttributes{len:3}, &indices, &program, &uniforms, &Default::default()).unwrap();
         target.finish().unwrap();
 
         events_loop.poll_events(|ev| {
             match ev {
                 glutin::Event::WindowEvent { event, .. } => match event {
-                    glutin::WindowEvent::CloseRequested => closed = true,
+                    glutin::WindowEvent::CloseRequested => is_running = false,
                     _ => (),
                 },
                 _ => (),
