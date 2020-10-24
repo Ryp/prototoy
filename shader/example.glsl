c4069e9e01b57bb891008e32f6ac1e0a08c0276e
Ryp <thibault.schueller@protonmail.com>
2020-10-24 17:53:59 +0200

    doc: comment out supported glsl builtins

--- shader/example.glsl
+++ shader/example.glsl
@@ -1,3 +1,17 @@
+// NOTE: Supported builtins
+// uniform vec3 iResolution;
+// uniform float iTime;
+
+// FIXME: Unsupported Shadertoy builtins
+// uniform float iTimeDelta;
+// uniform float iFrame;
+// uniform float iChannelTime[4];
+// uniform vec4 iMouse;
+// uniform vec4 iDate;
+// uniform float iSampleRate;
+// uniform vec3 iChannelResolution[4];
+// uniform samplerXX iChanneli;
+
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
     // Normalized pixel coordinates (from 0 to 1)
